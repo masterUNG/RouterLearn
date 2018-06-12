9a996a5847e299b6bf250879e5458cbd41a8f719
MasterUNG <phrombutr@gmail.com>
2018-06-12 14:05:18 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FirstcomponentComponent } from './components/firstcomponent/firstcomponent.component';
+import { SecondcomponentComponent } from './components/secondcomponent/secondcomponent.component';
+import { ThirdcomponentComponent } from './components/thirdcomponent/thirdcomponent.component';
+import { FourcomponentComponent } from './components/fourcomponent/fourcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcomponentComponent,
+    SecondcomponentComponent,
+    ThirdcomponentComponent,
+    FourcomponentComponent
   ],
   imports: [
     BrowserModule
